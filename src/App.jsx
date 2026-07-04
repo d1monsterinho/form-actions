4efc7b0b5ca7bb6b5403a3471ec0ed51f607c410
Dmytro Yevich <dmytro.yevich@cengage.com>
2026-07-04 15:23:19 +0300

    Merge pull request #1: Do reformat

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import Header from './components/Header.jsx';
 import Signup from './components/Signup.jsx';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <main>
-        <Signup />
-      </main>
-    </>
-  );
+    return (
+        <>
+            <Header/>
+            <main>
+                <Signup/>
+            </main>
+        </>
+    );
 }
 
 export default App;
